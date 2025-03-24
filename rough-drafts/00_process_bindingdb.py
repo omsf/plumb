9a99bcfc7668c946e1076a28545c0ca5261217a2
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-03-24 15:31:25 -0400

    update output paths for process binding db

--- rough-drafts/00_process_bindingdb.py
+++ rough-drafts/00_process_bindingdb.py
@@ -12,11 +12,15 @@ import json
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument('--input-dir', type=Path, help='SDF file to process')
+    parser.add_argument('--output-dir', type=Path, default="./", help='Directory to write output files')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
+
+    output_dir = args.output_dir
+    output_dir.mkdir(exist_ok=True, parents=True)
 
     # get all sdf files
     sdfs = list(args.input_dir.glob('*3D.sdf'))
@@ -44,28 +48,28 @@ def main():
 
             # write out sdf file
             if mol_dict['has_3d']:
-                mol.to_sdf(f'{mol.compound_name}.sdf')
+                mol.to_sdf(output_dir / f'{mol.compound_name}.sdf')
 
             output.append(mol_dict)
 
     df = pd.DataFrame.from_records(output)
-    df.to_csv('processed_bindingdb.csv', index=False)
+    df.to_csv(output_dir / 'processed_bindingdb.csv', index=False)
 
     # write separate csvs for 2D and 3D
     df_2d = df[df['has_3d']]
     df_3d = df[df['has_3d']]
-    df_2d.to_csv('2d_bindingdb.csv', index=False)
-    df_3d.to_csv('3d_bindingdb.csv', index=False)
+    df_2d.to_csv(output_dir / '2d_bindingdb.csv', index=False)
+    df_3d.to_csv(output_dir / '3d_bindingdb.csv', index=False)
 
     # get unique sdf filenames with 3D coordinates
     unique_sdf_filenames = df_3d['filename'].unique()
-    with open('unique_3D_sdf_filenames.txt', 'w') as f:
+    with open(output_dir / 'unique_3D_sdf_filenames.txt', 'w') as f:
         for filename in unique_sdf_filenames:
             f.write(f'{filename}\n')
 
     # write out separate json records
     for record in df_3d.to_dict(orient='records'):
-        with open(f'{record["compound_name"]}.json', 'w') as f:
+        with open(output_dir / f'{record["compound_name"]}.json', 'w') as f:
             f.write(json.dumps(record, indent=4))
 
 if __name__ == '__main__':
